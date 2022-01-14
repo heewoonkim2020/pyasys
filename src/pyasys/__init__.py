51b0e86ef5eff589ecb700beef1b93e0ee07c13d
Heewoon Kim <heewoonkim2020@gmail.com>
2022-01-14 14:14:32 +0900

    Added project class for structure definitions

---
+++ src/pyasys/__init__.py
@@ -0,0 +1,18 @@
+"""
+The official Pyasys Python library!
+
+Please check README.md for more information on how to set up Pyasys for your next project.
+"""
+
+class Pyasys:
+    def __init__(self, import_name):
+        """
+        Sets up a basic project class where project data can be stored.
+        
+        :param import_name: The __name__ of the import structure.
+        """
+        # Setup
+    
+    def prepare(self):
+        # Prepare for project
+        pass
