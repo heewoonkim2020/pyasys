f40e6dd1e839994da22b580903f2e44e3cd380ff
Heewoon Kim <heewoonkim2020@gmail.com>
2022-01-14 14:19:42 +0900

    Created project setup method

--- src/pyasys/__init__.py
+++ src/pyasys/__init__.py
@@ -5,14 +5,15 @@ Please check README.md for more information on how to set up Pyasys for your nex
 """
 
 class Pyasys:
-    def __init__(self, import_name):
+    def __init__(self, import_name, author):
         """
         Sets up a basic project class where project data can be stored.
         
         :param import_name: The __name__ of the import structure.
         """
         # Setup
+        self.setup_project(project_automation=True, author=author, import_dir=import_name)
     
-    def prepare(self):
+    def setup_project(self, project_automation, author, import_dir):
         # Prepare for project
-        pass
+        self.project_automation = project_automation
