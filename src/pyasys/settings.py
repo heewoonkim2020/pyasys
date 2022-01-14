b4fa29e3794ef0bd47aa8c3e37c37ec1966967e7
Heewoon Kim <heewoonkim2020@gmail.com>
2022-01-14 14:50:10 +0900

    Improved app system

--- src/pyasys/settings.py
+++ src/pyasys/settings.py
@@ -15,3 +15,14 @@ INSTALLED_COMPONENTS = [
     'pyasys',
     'pyasys.components.MIDDLEWARE'
 ]
+
+REQUIRED_APPS = [
+    'pyasys',
+    'pyasys.contrib',
+    'pyasys.contrib.auth',
+    'pyasys.contrib.models',
+]
+
+for required_app in REQUIRED_APPS:
+    if not required_app in INSTALLED_APPS:
+        INSTALLED_APPS.append(required_app)
